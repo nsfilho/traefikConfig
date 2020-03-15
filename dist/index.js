9e57459f838b6da94174167a1f65000ba33e6ea1
Nelio Santos <nelio.santos@jbtec.com.br>
2020-03-15 20:22:45 -0300

    feat: added auth, securityPolicy and chains in middleware

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const child_process_1 = require("child_process");
 const inquirer_1 = __importDefault(require("inquirer"));
 const commander_1 = __importDefault(require("commander"));
+const bcryptjs_1 = __importDefault(require("bcryptjs"));
 const containers_1 = require("./containers");
 const config_1 = require("./config");
 const buildQuestions = () => [
@@ -20,6 +21,12 @@ const buildQuestions = () => [
                 value: container.id,
             };
         }),
+    },
+    {
+        type: 'string',
+        name: 'entrypointHttp',
+        message: 'Nome do entrypoint http (não ssl)',
+        default: 'http',
     },
     {
         type: 'list',
@@ -40,11 +47,44 @@ const buildQuestions = () => [
             { name: 'Não', value: false },
         ],
         default: true,
+        when: (answers) => answers.ssl,
+    },
+    {
+        type: 'number',
+        name: 'portSSL',
+        message: 'Porta do Scheme SSL',
+        default: 443,
+        when: (answers) => answers.ssl,
+    },
+    {
+        type: 'string',
+        name: 'entrypointHttps',
+        message: 'Nome do entrypoint do SSL',
+        default: 'https',
+        when: (answers) => answers.ssl,
+    },
+    {
+        type: 'list',
+        name: 'certResolver',
+        message: 'Utiliza certResolver (ACME)',
+        default: true,
+        choices: [
+            { name: 'Sim', value: true, checked: true },
+            { name: 'Não', value: false, checked: false },
+        ],
+        when: (answers) => answers.ssl,
+    },
+    {
+        type: 'string',
+        name: 'certResolverName',
+        message: 'Nome do seu certResolver (ACME)',
+        default: 'mySSL',
+        when: (answers) => answers.certResolver,
     },
     {
         type: 'number',
         name: 'port',
-        message: 'Porta do serviço',
+        message: 'Porta do serviço na container',
         default: 80,
     },
     {
@@ -68,6 +108,33 @@ const buildQuestions = () => [
         name: 'hosts',
         message: 'Digite os hosts (um por linha)',
         default: '',
+    },
+    {
+        type: 'list',
+        name: 'auth',
+        message: 'Deseja habilitar autenticação (basic)',
+        choices: [
+            { name: 'Sim', value: true, checked: false },
+            { name: 'Não', value: false, checked: true },
+        ],
+        default: false,
+    },
+    {
+        type: 'editor',
+        name: 'authUsers',
+        message: 'Digite em cada linha <usuario>:<senha>',
+        default: 'admin:password',
+        when: (answers) => answers.auth,
+    },
+    {
+        type: 'list',
+        name: 'securityPolicy',
+        message: 'Deseja habilitar uma custom Security-Policy',
+        default: false,
+        choices: [
+            { name: 'Sim', value: true, checked: false },
+            { name: 'Não', value: false, checked: true },
+        ],
     },
     {
         type: 'list',
@@ -96,28 +163,65 @@ const execute = () => {
         const hostConfig = hosts.reduce((acc, cur, idx) => `${acc}Host(\\\`${cur}\\\`)${idx < hosts.length - 1 ? '||' : ''}`, '');
         const dupadd = (lbl) => labelAdd.push(`--label-add "${lbl}"`);
         const duprm = (lbl) => labelRm.push(`--label-rm "${lbl}"`);
+        const middlewaresHttp = [];
+        const middlewaresHttps = [];
+        const sMiddle = `traefik.http.middlewares.${containerNameDashed}`;
+        const sRoute = `traefik.http.routers.${containerNameDashed}`;
         // Prepara os comandos
         labels.forEach(v => duprm(v.key));
         dupadd('traefik.enable=true');
         dupadd('traefik.docker.network=proxy');
         dupadd(`traefik.http.services.${containerNameDashed}.loadbalancer.server.port=${answers.port}`);
         if (answers.ssl) {
-            dupadd(`traefik.http.routers.${containerNameDashed}-secure.entrypoints=https`);
-            dupadd(`traefik.http.routers.${containerNameDashed}-secure.rule=${hostConfig}`);
-            dupadd(`traefik.http.routers.${containerNameDashed}-secure.service=${containerNameDashed}`);
-            dupadd(`traefik.http.routers.${containerNameDashed}-secure.tls=true`);
-            dupadd(`traefik.http.routers.${containerNameDashed}-secure.tls.certresolver=mySSL`);
-            dupadd(`traefik.http.routers.${containerNameDashed}-secure.priority=${answers.priority}`);
+            dupadd(`${sRoute}-secure.entrypoints=${answers.entrypointHttps}`);
+            dupadd(`${sRoute}-secure.rule=${hostConfig}`);
+            dupadd(`${sRoute}-secure.service=${containerNameDashed}`);
+            dupadd(`${sRoute}-secure.tls=true`);
+            if (answers.certResolver)
+                dupadd(`${sRoute}-secure.tls.certresolver=${answers.certResolverName}`);
+            dupadd(`${sRoute}-secure.priority=${answers.priority}`);
         }
-        dupadd(`traefik.http.routers.${containerNameDashed}.entrypoints=http`);
-        dupadd(`traefik.http.routers.${containerNameDashed}.rule=${hostConfig}`);
-        dupadd(`traefik.http.routers.${containerNameDashed}.priority=${answers.priority}`);
+        dupadd(`${sRoute}.entrypoints=${answers.entrypointHttp}`);
+        dupadd(`${sRoute}.rule=${hostConfig}`);
+        dupadd(`${sRoute}.priority=${answers.priority}`);
         if (answers.ssl && answers.sslredirect) {
-            dupadd(`traefik.http.routers.${containerNameDashed}.middlewares=${containerNameDashed}-https-redirect`);
-            dupadd(`traefik.http.middlewares.${containerNameDashed}-https-redirect.redirectscheme.scheme=https`);
+            middlewaresHttp.push(`${containerNameDashed}-https-redirect`);
+            dupadd(`${sMiddle}-https-redirect.redirectscheme.scheme=https`);
+            dupadd(`${sMiddle}-https-redirect.redirectscheme.port=${answers.portSSL}`);
         }
         if (answers.sticky) {
             dupadd(`traefik.http.services.${containerNameDashed}.loadbalancer.sticky=true`);
+        }
+        if (answers.auth) {
+            const encryptedUsers = answers.authUsers
+                .split('\n')
+                .filter(l => l.trim().length > 0)
+                .map(value => {
+                const [user, password] = value.split(':');
+                console.log(value);
+                dupadd(`traefikConfig.clearPassword.${user}=${password}`);
+                return `${user}:${bcryptjs_1.default.hashSync(password).replace(/\$/g, '\\$')}`;
+            });
+            dupadd(`${sMiddle}-auth.basicauth.users=${encryptedUsers.join(',')}`);
+            if (!answers.sslredirect)
+                middlewaresHttp.push(`${containerNameDashed}-auth`);
+            if (answers.ssl)
+                middlewaresHttps.push(`${containerNameDashed}-auth`);
+        }
+        if (answers.securityPolicy) {
+            const dominios = hosts.map(v => `http://${v} https://${v}`).join(' ');
+            dupadd(`${sMiddle}-secPolicy.headers.customresponseheaders.Content-Security-Policy=connect-src ${dominios} 'self'`);
+            if (answers.ssl)
+                middlewaresHttps.push(`${containerNameDashed}-secPolicy`);
+            middlewaresHttp.push(`${containerNameDashed}-secPolicy`);
+        }
+        if (middlewaresHttp.length > 0) {
+            dupadd(`${sMiddle}-chain.chain.middlewares=${middlewaresHttp.join(',')}`);
+            dupadd(`${sRoute}.middlewares=${containerNameDashed}-chain`);
+        }
+        if (middlewaresHttps.length > 0) {
+            dupadd(`${sMiddle}-secure-chain.chain.middlewares=${middlewaresHttps.join(',')}`);
+            dupadd(`${sRoute}-secure.middlewares=${containerNameDashed}-secure-chain`);
         }
         // Adiciona o comando das labels
         cmds.push(`docker service update \\\n${labelRm.join(' \\\n')} \\\n${containerName}`);
